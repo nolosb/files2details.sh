90b3cac7f3f0449bc5441057a7f318839b03ed7f
rs882k <rs882k@login.rip>
2018-03-09 14:44:31 +0000

    initial commit

---
+++ files2details.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR_IN="files"
+
+for FILE in $DIR_IN/*; do
+	DETAILS="$DETAILS \
+	<details> \
+	<summary onclick='closeOpen()'>$(basename $FILE)</summary> \
+	<div> $(txt2html --extract --capstag '' --preserve_indent $FILE) </div> \
+	</details>"  
+done
+
+echo "$DETAILS" > details.html
+
---
+++ includeHTML.js
@@ -0,0 +1,25 @@
+function includeHTML () {
+	var z, i, element, file, xhttp
+	z = document.getElementsByTagName("*")
+	for (i=0; i < z.length; i++) {
+		element = z[i]
+		file = element.getAttribute("w3-include-html")
+		if (file) {
+			xhttp = new XMLHttpRequest()
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4){
+					if (this.status == 200) {
+						element.innerHTML = this.responseText;}
+					if (this.status == 404) {
+						element.innerHTML = "Page not found"}
+					element.removeAttribute("w3-include-html")
+					includeHTML();
+				}
+			}
+			xhttp.open("GET", file, true)
+			xhttp.send()
+			return
+		}
+	}
+}
+
