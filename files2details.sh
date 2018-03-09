48ddbdd80b56e95ddfc8a64a142a77873d0770b0
rs882k <26887899+rs882k@users.noreply.github.com>
2018-03-09 15:30:44 +0000

    inital commit

--- files2details.sh
+++ files2details.sh
@@ -5,7 +5,7 @@ DIR_IN="files"
 for FILE in $DIR_IN/*; do
 	DETAILS="$DETAILS \
 	<details> \
-	<summary onclick='closeOpen()'>$(basename $FILE)</summary> \
+	<summary>$(basename $FILE)</summary> \
 	<div> $(txt2html --extract --capstag '' --preserve_indent $FILE) </div> \
 	</details>"  
 done
